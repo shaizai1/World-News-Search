1be821acd776ad3381b339a349ef11bcb4976263
Zee Mudia <iosazee1@gmail.com>
2023-02-02 16:34:58 +0000

    added html skeleton for news section

---
+++ assets/Js/script.js
@@ -0,0 +1,10 @@
+/*
+* Make two fetch calls and get the json data on the console
+* Sort through the data and display relevant portions
+* Dynamically generate the bootstrap cards for displaying the news
+* Add an onchange event listener to the select element so that when
+** the user selects an option the cards are generated and the news
+*** is displayed.
+*/
+
+

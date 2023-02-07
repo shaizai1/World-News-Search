19ea7de6460de51574b0afe52d1f2a31bd2c9ade
Alex Ainslie <alex.ainslie99@gmail.com>
2023-02-07 15:06:32 +0000

    Slight edits

--- assets/Js/country-info.js
+++ assets/Js/country-info.js
@@ -1,13 +1,9 @@
-var countryName = ""
+var dropdownElement = document.getElementById("country-dropdown");
 
-if(countryName == ""){
-    document.getElementById("countryInfoText").innerHTML = "";
-    document.getElementById("countryInfoText").innerHTML = `
-    <p>Please Select a Country</p>`;
-    document.getElementById("countryInfoText").setAttribute("class","d-flex align-items-center justify-content-center");
-    document.getElementById("countryInfoText").setAttribute("style","height: 200px");
-}
-else{
+
+dropdownElement.addEventListener('change', function(){
+    var countryName = dropdownElement.value
+
     var queryURL = `https://restcountries.com/v3.1/name/${countryName}`
 
 
@@ -17,20 +13,20 @@ else{
 
         console.log(country)
     
-        var flagURL = country[0].flags.svg;
-        var flagAlt = country[0].flags.alt;
+        let flagURL = country[0].flags.svg;
+        let flagAlt = country[0].flags.alt;
         document.getElementById("flags").setAttribute("src", flagURL);
         document.getElementById("flags").setAttribute("style","max-width: 300px");
         document.getElementById("flags").setAttribute("alt",flagAlt)
 
-        var name = country[0].name.common;
+        let name = country[0].name.common;
         document.getElementById("titleCountry").textContent = name;
 
         // Variables for main Information
-        var capital = country[0].capital[0];
-        var continent = country[0].region;
-        var currency = country[0].currencies;
-        var languages = country[0].languages;
+        let capital = country[0].capital[0];
+        let continent = country[0].region;
+        let currency = country[0].currencies;
+        let languages = country[0].languages;
 
         // Breakdown of currency
         var currencyCode = Object.keys(currency).toString();
@@ -66,11 +62,12 @@ else{
 
 
         // Getting map link from API
-        var mapLink = `https://maps.google.com/maps?q=${countryName}&output=embed`
+        var mapLink = `https://maps.google.com/maps?q=${countryName}&output=embed`;
         document.getElementById("modalMapElement").setAttribute("src", mapLink);
         
         // Modal Info
         var modalHeading = `Map of ${name}`;
         document.getElementById("modalHeader").textContent = modalHeading;
     })
-}
+})
+

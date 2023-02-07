7cc934f2d12cb9a049a1bbf5339021100150ffec
Zee Mudia <iosazee1@gmail.com>
2023-02-07 15:51:29 +0000

    merged news-js into country-info-js

--- assets/Js/country-info.js
+++ assets/Js/country-info.js
@@ -1,8 +1,12 @@
 var dropdownElement = document.getElementById("country-dropdown");
+let apiKey = "4cdf6c2ecb724b9a939c3f53081d9566"
+let mainEl = document.querySelector('#main')
+let newsEl = document.querySelector('#news')
 
 
 dropdownElement.addEventListener('change', function(){
     var countryName = dropdownElement.value
+    var selectedCountry = dropdownElement.value
 
     var queryURL = `https://restcountries.com/v3.1/name/${countryName}`
 
@@ -10,9 +14,8 @@ dropdownElement.addEventListener('change', function(){
     fetch(queryURL)
     .then(response => response.json())
     .then(function(country){
-
+        let countryCode = country[0].cca2
         console.log(country)
-    
         let flagURL = country[0].flags.svg;
         let flagAlt = country[0].flags.alt;
         document.getElementById("flags").setAttribute("src", flagURL);
@@ -41,11 +44,11 @@ dropdownElement.addEventListener('change', function(){
             var currencyName = country[0].currencies[currencyCode].name;
             var currencySymbol = country[0].currencies[currencyCode].symbol;
         }
-  
+
         // Breakdown to access all languages
         var trial = Object.keys(languages);
         var languageList = [];
-        
+
         for(i=0;i<trial.length;i++){
             var languageName = country[0].languages[trial[i]]
             languageList.push(languageName);
@@ -64,10 +67,71 @@ dropdownElement.addEventListener('change', function(){
         // Getting map link from API
         var mapLink = `https://maps.google.com/maps?q=${countryName}&output=embed`;
         document.getElementById("modalMapElement").setAttribute("src", mapLink);
-        
+
         // Modal Info
         var modalHeading = `Map of ${name}`;
         document.getElementById("modalHeader").textContent = modalHeading;
+        return fetch(`https://newsapi.org/v2/top-headlines/sources?country=${countryCode}&apiKey=${apiKey}`)
+
     })
+    .then(response => response.json())
+   .then(data => {
+      console.log("data2",data)
+
+      const arrays = data.sources
+      console.log("arrays",arrays)
+
+        const newsResults =  arrays.map((item) => {
+          const newsHeading = item.name
+          const newsUrl = item.url
+          const description = item.description
+          const card = document.createElement('div')
+          card.innerHTML =  `<div class="news-card">
+                                <h5 class="card-title">${newsHeading}</h5>
+                                <a href=${newsUrl} class="card-link" target="_blank">${newsUrl} news</a>
+                                <p class="card-text">${description}</p>
+                              </div>`
+
+          return card
+        })
+
+         const googleSearch = () => {
+
+           const altUrl = `https://news.google.com/search?q=${selectedCountry}%20news&hl=en-GB&gl=GB&ceid=GB%3Aen`
+            console.log(altUrl)
+            const card = document.createElement('div')
+            card.innerHTML =  `<div class="news-card">
+                                <h5 class="card-title"> Google News ${selectedCountry} </h5>
+                                <a href=${altUrl} class="card-link" target="_blank">Click here for ${selectedCountry} news</a>
+                                <p class="card-text">Google News from ${selectedCountry}</p>
+                              </div>`
+
+            return newsEl.append(card)
+         }
+
+        console.log("results",newsResults)
+
+        if (arrays.length !== 0) {
+
+          newsResults.map((card) =>{
+
+           newsEl.append(card)
+
+
+          })
+        } else {
+          googleSearch()
+        }
+    });
 })
 
+
+// mainEl.addEventListener('change', (e) => {
+
+
+//     if(e.target.id === "country-dropdown") {
+//       const option = e.target.value
+//       newsSearch(e, option)
+//     }
+
+//   })
